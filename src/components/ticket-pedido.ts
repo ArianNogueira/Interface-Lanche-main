69ec66a80311cb8d8d206ac4b2600753bd8f29f7
Arian <arianviegas12@gmail.com>
2024-12-10 13:58:18 -0300

    feat: initializing with typescript

---
+++ src/components/ticket-pedido.ts
@@ -0,0 +1,17 @@
+const adiconarItem = document.querySelector(".adicionar-item");
+
+adiconarItem?.addEventListener("click", () => {
+  alert("teste");
+})
+
+function rederinzarTicketPedidos() {
+
+}
+
+const ExtratoPedido = {
+  atualizar(): void {
+    rederinzarTicketPedidos();
+  }
+}
+
+export default ExtratoPedido;
